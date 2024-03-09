874cf3b51580ff8ce299a2e38d686f47e16b166c
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-09 04:51:29 +0100

    Create CloseStore.java

---
+++ src/Event/CloseStore.java
@@ -0,0 +1,37 @@
+// Amadeus Färdig
+
+package Event;
+
+import SimState.Customer;
+import SimState.SimState;
+
+public class CloseStore extends Event {
+	private double time;
+
+	public CloseStore(SimState state, EventQueue eventQueue, double time) {
+		super(state, eventQueue);
+		this.time = time;
+	}
+
+	@Override
+	public void doMe() {
+		state.update(this);
+		state.getStore().setIsOpen(false);
+	}
+
+	@Override
+	public double getTime() {
+		return time;
+	}
+
+	@Override
+	public String getName() {
+		return "Stänger";
+	}
+
+	@Override
+	public Customer getCustomer() {
+		return null;
+	}
+
+}
