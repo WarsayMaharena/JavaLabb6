904e370e13da792568bf10179e6f6811c548bc89
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-09 04:52:37 +0100

    Update Pay.java

--- src/Event/Pay.java
+++ src/Event/Pay.java
@@ -1,5 +1,48 @@
+// Amadeus Färdig
+
 package Event;
 
-public class Pay {
+import SimState.*;
+
+public class Pay extends Event {
+	private Customer customer;
+	private double time;
+
+	public Pay(SimState state, EventQueue eventQueue, double time, Customer customer) {
+		super(state, eventQueue);
+		this.customer = customer;
+		this.time = time;
+	}
+
+	@Override
+	public void doMe() {
+		state.update(this);
+		state.getStore().addMoney();
+		state.getStore().removeCustomer(customer);
+		state.getStore().releaseRegister();
+
+		if (!state.getStore().getFIFO().isEmpty()) {
+			Customer nextCustomer = state.getStore().getFIFO().poll();
+			double nextPayTime = this.time + state.getPayTime().next();
+			Pay nextPayEvent = new Pay(this.state, this.eventQueue, nextPayTime, nextCustomer);
+			eventQueue.addEvent(nextPayEvent);
+			state.getStore().occupyRegister();
+		}
+	}
+
+	@Override
+	public double getTime() {
+		return time;
+	}
+
+	@Override
+	public String getName() {
+		return "Betalning";
+	}
+
+	@Override
+	public Customer getCustomer() {
+		return customer;
+	}
 
 }
