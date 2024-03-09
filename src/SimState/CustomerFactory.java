196ef2e0f496db4649da90139b6db54155d20e30
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-09 04:48:16 +0100

    Update CustomerFactory.java

--- src/SimState/CustomerFactory.java
+++ src/SimState/CustomerFactory.java
@@ -1,14 +1,12 @@
+// Amadeus Färdig
+
 package SimState;
 
 
 public class CustomerFactory {
-		
-	private int customersCreated = 0;
 
-	public Customer createCustomer()
-	   {
-	      Customer customer = new Customer(customersCreated);
-	      customersCreated += 1;
-	      return customer;
-	   }
+
+	public Customer createCustomer() {
+		return new Customer(); // Customer ID is now managed internally by the Customer class.
+	}
 }
