8b1b0c6ce2100d32dfdce4af6b63b4b101e62f92
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-09 04:48:07 +0100

    Update Customer.java

--- src/SimState/Customer.java
+++ src/SimState/Customer.java
@@ -1,12 +1,42 @@
+// Amadeus Färdig
+
 package SimState;
 
+
+
+
 public class Customer {
-	
-	private int id;
-	private String state;
-	
-	public Customer(int id){
-		this.id = id;
+	private static int nextId = -1;
+    private int customerID;
+    private CustomerState state;
+
+
+	public Customer() {
+		this.customerID = ++nextId;
+		this.state = CustomerState.notInStore;
+	}
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public CustomerState getState() {
+		return state;
+	}
+
+	public void setState(CustomerState state) {
+		this.state = state;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(customerID);
+		return sb.toString();
+	}
+
+	public enum CustomerState {
+		inStore, notInStore, turnedAway, lateCustomer
 	}
 
 }
