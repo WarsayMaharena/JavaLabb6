ca18dd1e38c73a6ce9b2f9f73e73268cabedb56b
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-10 07:58:21 +0100

    Update View.java

--- src/View/View.java
+++ src/View/View.java
@@ -1,25 +1,48 @@
-// Amadeus Färdig
-
 package View;
 
 import java.util.Observable;
 import java.util.Observer;
 
-import SimState.SimState;
-import SimState.Store;
+import State.State;
+import State.Store;
 
+/**
+ * En abstrakt bas för alla vy-klasser i simuleringen som implementerar Observer
+ * gränssnittet. Denna klass fungerar som en brygga mellan simuleringens
+ * tillstånd och dess presentation till användaren, genom att lyssna på
+ * förändringar i simuleringens tillstånd och uppdatera användargränssnittet
+ * därefter. Varje konkret subklass av View måste implementera update() metoden
+ * för att definiera hur vy-uppdateringar hanteras när observerade objekt
+ * notifierar om förändringar. Konstruktorn tar emot en referens till
+ * simuleringens tillstånd och en butik, och registrerar vy-objektet som en
+ * observatör till tillståndet, vilket möjliggör automatiska uppdateringar vid
+ * förändringar.
+ * 
+ * @param state En referens till simuleringens tillstånd som denna vy kommer att
+ *              observera.
+ * @param store En referens till butiken som denna vy kommer att presentera
+ *              information om.
+ */
+@SuppressWarnings("deprecation")
 public abstract class View implements Observer {
 
-    protected SimState state;
-    protected Store store;
+	State state;
+	Store store;
 
-    public View(SimState state, Store store) {
-        this.state = state;
-        this.store = store;
-        state.addObserver(this);
-    }
+	public View(State state, Store store) {
+		state.addObserver(this);
+	}
 
+	/**
+	 * Hanterar notifieringar om uppdateringar från observerade objekt. Konkreta
+	 * implementationer av denna metod ska definiera hur vy-uppdateringar genomförs
+	 * baserat på förändringar i simuleringens tillstånd.
+	 * 
+	 * @param o   Det observerade objektet som notifierar om förändring.
+	 * @param arg Ett argument skickat av det observerade objektet, kan användas för
+	 *            att föra över specifik information.
+	 */
+	@Override
+	abstract public void update(Observable o, Object arg);
 
-    @Override
-    abstract public void update(Observable o, Object arg);
 }
