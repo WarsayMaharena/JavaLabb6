a589973b9d301e07240eacbe034c33754c5e95f2
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-10 07:45:25 +0100

    Update SnabbkopsState.java

--- src/SimState/SnabbkopsState.java
+++ src/SimState/SnabbkopsState.java
@@ -1,9 +1,16 @@
-//Amadeus UPPDATERA CHANGEREGISTERAVAILABILITY I GITHUB OCH GETCUSTOMERS
-
 package SimState;
 
 import java.util.ArrayList;
 
+/**
+ * Representerar tillståndet för en snabbköpssimulation. Denna klass hanterar
+ * snabbköpets logiska tillstånd, inklusive kunder, kassor och öppettider. Den
+ * tillhandahåller metoder för att interagera med snabbköpet, som att öppna
+ * eller stänga det, hantera kundflödet och uppdatera tillstånd baserat på
+ * händelser i simuleringen.
+ * 
+ * @author Amadeus Olofsson, Warsay Maharena, Hjalmar Norén
+ */
 public class SnabbkopsState {
 	private boolean isOpen = false;
 	private final int maxCustomers;
@@ -22,6 +29,15 @@ public class SnabbkopsState {
 	private int totalCustomersInQueue = 0;
 	private double lastPaymentTime = 0.0;
 
+	/**
+	 * Skapar en ny instans av SnabbkopsState. Initierar snabbköpet med angivet
+	 * antal maximala kunder, kassor och stängningstid.
+	 * 
+	 * @param maxCustomers Maximalt antal kunder som får vara i snabbköpet
+	 *                     samtidigt.
+	 * @param registers    Antal tillgängliga kassor.
+	 * @param closingTime  Tiden då snabbköpet stänger.
+	 */
 	public SnabbkopsState(int maxCustomers, int registers, double closingTime) {
 		this.maxCustomers = maxCustomers;
 		this.registers = registers;
@@ -29,46 +45,102 @@ public class SnabbkopsState {
 		this.freeRegisters = registers;
 	}
 
+	/**
+	 * Kontrollerar om snabbköpet är öppet.
+	 * 
+	 * @return true om snabbköpet är öppet, annars false.
+	 */
 	public boolean isOpen() {
 		return isOpen;
 	}
 
+	/**
+	 * Ställer in snabbköpets öppettillstånd.
+	 * 
+	 * @param isOpen true för att öppna snabbköpet, false för att stänga det.
+	 */
 	public void setOpen(boolean isOpen) {
 		this.isOpen = isOpen;
 	}
 
+	/**
+	 * Hämtar det maximala antalet kunder som tillåts i snabbköpet samtidigt.
+	 * 
+	 * @return Maximalt antal kunder.
+	 */
 	public int getMaxCustomers() {
 		return maxCustomers;
 	}
 
+	/**
+	 * Hämtar antalet kassor i snabbköpet.
+	 * 
+	 * @return Antalet kassor.
+	 */
 	public int getRegisters() {
 		return registers;
 	}
 
+	/**
+	 * Hämtar tiden då snabbköpet stänger.
+	 * 
+	 * @return Stängningstiden.
+	 */
 	public double getClosingTime() {
 		return closingTime;
 	}
 
+	/**
+	 * Hämtar den totala mängden pengar som snabbköpet har tjänat.
+	 * 
+	 * @return Totala mängden pengar.
+	 */
 	public int getTotalMoney() {
 		return totalMoney;
 	}
 
+	/**
+	 * Hämtar antalet lediga kassor.
+	 * 
+	 * @return Antalet lediga kassor.
+	 */
 	public int getFreeRegisters() {
 		return freeRegisters;
 	}
 
+	/**
+	 * Hämtar den totala tiden kassorna har varit lediga.
+	 * 
+	 * @return Tiden kassorna har varit lediga.
+	 */
 	public double getRegisterFreeTime() {
 		return registerFreeTime;
 	}
 
+	/**
+	 * Hämtar den totala tiden kunder har tillbringat i kö.
+	 * 
+	 * @return Tiden kunder har tillbringat i kö.
+	 */
 	public double getCustomerQueueTime() {
 		return customerQueueTime;
 	}
 
+	/**
+	 * Hämtar kölinjen till kassorna.
+	 * 
+	 * @return Kölinjen som ett FIFO-objekt.
+	 */
 	public FIFO getRegisterLine() {
 		return registerLine;
 	}
 
+	/**
+	 * Ändrar tillgängligheten för kassorna baserat på om en kassa ska upptas eller
+	 * frigöras.
+	 * 
+	 * @param occupy Sant för att uppta en kassa, falskt för att frigöra en.
+	 */
 	public void changeRegisterAvailability(boolean occupy) {
 		if (occupy && freeRegisters > 0) {
 			freeRegisters--;
@@ -77,11 +149,21 @@ public class SnabbkopsState {
 		}
 	}
 
+	/**
+	 * Skapar en ny kund via kundfabriken.
+	 * 
+	 * @return Den nyskapade kunden.
+	 */
 	public Customer createNewCustomer() {
-		Customer customer = customerFactory.createCustomer();
-		return customer;
+		return customerFactory.createCustomer();
 	}
 
+	/**
+	 * Lägger till en kund i snabbköpet och ställer in kundens tillstånd baserat på
+	 * snabbköpets status.
+	 * 
+	 * @param customer Kunden som ska läggas till.
+	 */
 	public void addCustomer(Customer customer) {
 		if (!isOpen) {
 			customer.setState(Customer.CustomerState.lateCustomer);
@@ -93,46 +175,99 @@ public class SnabbkopsState {
 		customers.add(customer);
 	}
 
+	/**
+	 * Räknar antalet kunder i snabbköpet.
+	 * 
+	 * @return Antalet kunder som är inne i snabbköpet.
+	 */
 	public int getCustomersInStore() {
 		return (int) customers.stream().filter(c -> c.getState() == Customer.CustomerState.inStore).count();
 	}
 
+	/**
+	 * Räknar antalet kunder som har nekats tillträde till snabbköpet.
+	 * 
+	 * @return Antalet kunder som har nekats tillträde.
+	 */
 	public int getCustomersTurnedAway() {
 		return (int) customers.stream().filter(c -> c.getState() == Customer.CustomerState.turnedAway).count();
 	}
 
+	/**
+	 * Räknar det totala antalet kunder som har besökt snabbköpet, exklusive de som
+	 * kommit efter stängning.
+	 * 
+	 * @return Det totala antalet besökande kunder.
+	 */
 	public int getTotalCustomers() {
 		return (int) customers.stream().filter(c -> c.getState() != Customer.CustomerState.lateCustomer).count();
 	}
 
+	/**
+	 * Ökar den totala mängden pengar snabbköpet har tjänat med en enhet.
+	 */
 	public void addMoney() {
 		totalMoney += 1;
 	}
 
+	/**
+	 * Tar bort en kund från snabbköpet och uppdaterar kundens tillstånd till ej
+	 * längre i butiken.
+	 * 
+	 * @param customer Kunden som ska tas bort.
+	 */
 	public void removeCustomer(Customer customer) {
 		customer.setState(Customer.CustomerState.notInStore);
 	}
 
+	/**
+	 * Uppdaterar den totala tiden kassorna har varit lediga.
+	 * 
+	 * @param time Den nya totala lediga tiden för kassorna.
+	 */
 	public void setRegisterFreeTime(double time) {
 		registerFreeTime = time;
 	}
 
+	/**
+	 * Uppdaterar den totala tiden kunder har tillbringat i kö.
+	 * 
+	 * @param time Den nya totala kötiden för kunder.
+	 */
 	void setCustomerQueueTime(double time) {
 		customerQueueTime = time;
 	}
 
+	/**
+	 * Hämtar det totala antalet kunder som har stått i kö.
+	 * 
+	 * @return Totala antalet kunder som har köat.
+	 */
 	public int getTotalCustomersInQueue() {
 		return totalCustomersInQueue;
 	}
 
+	/**
+	 * Ökar antalet kunder som totalt har köat med ett.
+	 */
 	public void addTotalCustomersInQueue() {
-		totalCustomersInQueue += 1;
+		totalCustomersInQueue++;
 	}
 
+	/**
+	 * Hämtar tiden för den senaste betalningen.
+	 * 
+	 * @return Tiden för den senaste betalningen.
+	 */
 	public double getLastPaymentTime() {
 		return lastPaymentTime;
 	}
 
+	/**
+	 * Sätter tiden för den senaste betalningen.
+	 * 
+	 * @param lastPaymentTime Den nya tiden för den senaste betalningen.
+	 */
 	public void setLastPaymentTime(double lastPaymentTime) {
 		this.lastPaymentTime = lastPaymentTime;
 	}
