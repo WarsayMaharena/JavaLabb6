b920560ffa5f216dd7f081e574385f8c2726920b
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-10 07:18:51 +0100

    Update Pay.java

--- src/Event/Pay.java
+++ src/Event/Pay.java
@@ -1,11 +1,30 @@
-//Amadeus UPPDATERA CHANGEREGISTERAVAILABILITY I GITHUB
+
 package Event;
 
 import SimState.*;
 
+/**
+ * Representerar en betalningshändelse i simuleringen.
+ *
+ * Denna händelse inträffar när en kund genomför sin betalning i kassan. Det
+ * uppdaterar simuleringens tillstånd genom att registrera betalningen, frigöra
+ * en kassaregister och, om det finns kö, initiera nästa kunds betalning.
+ * 
+ * @author Amadeus Olofsson, Warsay Maharena, Hjalmar Norén
+ */
+
 public class Pay extends Event {
 	private Customer customer;
 	private double time;
+
+	/**
+	 * Skapar en ny betalningshändelse.
+	 * 
+	 * @param state      Simuleringens tillstånd.
+	 * @param eventQueue Händelsekön.
+	 * @param time       Tidpunkten då betalningen sker.
+	 * @param customer   Kunden som betalar.
+	 */
 
 	public Pay(SimState state, EventQueue eventQueue, double time, Customer customer) {
 		super(state, eventQueue);
@@ -13,32 +32,54 @@ public class Pay extends Event {
 		this.time = time;
 	}
 
+	/**
+	 * Utför betalningshändelsen.
+	 *
+	 * Registrerar kundens betalning, frigör en kassaregister och, om det finns kö,
+	 * initierar nästa kunds betalning.
+	 *
+	 */
 	@Override
 	public void doMe() {
 		state.update(this);
-		state.getStore().addMoney();
+		state.getStore().addMoney(); // Anta att varje betalning ökar butikens intäkter med 1 enhet.
 		state.getStore().removeCustomer(customer);
-		 state.getStore().changeRegisterAvailability(false);
+		state.getStore().changeRegisterAvailability(false); // Ockupera en kassaregister.
 
 		if (!state.getStore().getRegisterLine().isEmpty()) {
 			Customer nextCustomer = state.getStore().getRegisterLine().poll();
 			double nextPayTime = this.time + state.getPayTime().next();
 			Pay nextPayEvent = new Pay(this.state, this.eventQueue, nextPayTime, nextCustomer);
 			eventQueue.addEvent(nextPayEvent);
-			state.getStore().changeRegisterAvailability(true);
+			state.getStore().changeRegisterAvailability(true); // Frigör en kassaregister för nästa kund.
 		}
 	}
 
+	/**
+	 * Hämtar tiden för händelsen.
+	 * 
+	 * @return Tiden då betalningen sker.
+	 */
 	@Override
 	public double getTime() {
 		return time;
 	}
 
+	/**
+	 * Hämtar händelsens namn.
+	 * 
+	 * @return En sträng som representerar händelsens typ.
+	 */
 	@Override
 	public String getName() {
 		return "Betalning";
 	}
 
+	/**
+	 * Hämtar kunden som är involverad i händelsen.
+	 * 
+	 * @return Kunden som betalar.
+	 */
 	@Override
 	public Customer getCustomer() {
 		return customer;
