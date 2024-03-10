cf610d66ce9c06d743fb0ae882ab76d30953a839
sleepynoodle <44613198+sleepynoodle@users.noreply.github.com>
2024-03-10 06:52:42 +0100

    Update Simulator.java

--- src/Simulator/Simulator.java
+++ src/Simulator/Simulator.java
@@ -1,14 +1,28 @@
 package Simulator;
 
-
+import Event.*;
 import SimState.SimState;
+import View.*;
 
 public class Simulator {
-	
-	SimState state = new SimState();
 
-	public static void main(String args[]) {
-
+	private SimState state;
+	private EventQueue eventQueue;
+	public Simulator(SimState state, View view) {
+		
+		this.state = state;
+		this.eventQueue = new EventQueue();
 	}
-	
+
+	public void run() {
+
+		eventQueue.addEvent(new SimStart(state, eventQueue));
+		eventQueue.addEvent(new SimStop(state, eventQueue));
+
+		while (state.getSimRunning()) {
+			Event event = eventQueue.getFirst();
+			eventQueue.removeFirst();
+			event.doMe();
+		}
+	}
 }
